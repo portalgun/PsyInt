78a542da75dc8804d5b8c606689eafdbfe4914c1
Dave White <d>
2022-02-14 11:22:23 -0500

    Automate iteration

--- PsyInt.m
+++ PsyInt.m
@@ -1,15 +1,22 @@
 classdef PsyInt < handle
 properties
-    Int
+    INTS
     nums
-    pos
+
+    s
+    int
+    trl
+
+    cOpts
+    name
+    bLastInit
 end
 methods
     function obj=PsyInt(fname)
         %fname='/home/dambam/Documents/MATLAB/.px/prj/DSP2/_def/D_psy_DSP2.m';
         if nargin < 1 || isempty(fname)
             d=Dir.parent(mfilename('fullpath'));
-            fname=[d 'D_int_default.cfg'];
+            fname=[d 'def/D_int_default.cfg'];
         end
         if ~Fil.exist(fname)
             error('file does not exist')
@@ -18,24 +25,67 @@ methods
         obj.parse(opts);
     end
     function n=getNSub(obj,int)
-        opts=obj.Int{obj.nums==int};
+        opts=obj.INTS{obj.nums==int};
         n=numel(fieldnames(opts));
     end
-    function [opts,name]=getSubOpts(obj,int,subIntORname)
-        if isempty(int);
-            int=1;
+    function [s,int,trl,opts,name]=inc(obj,bInit)
+        if nargin < 2
+            bInit=false;
         end
-        if isempty(subIntORname)
+        bSame=false;
+        if bInit
+            obj.s=1;
+            obj.int=1;
+            obj.trl=1;
+        elseif obj.s+1 <= obj.nums(obj.int)
+            obj.s=obj.s+1;
+        elseif obj.int+1 <= length(obj.INTS)
+            obj.s=1;
+            obj.int=obj.int+1;
+        elseif obj.cOpts.autoInc
+            obj.s=1;
+            obj.int=1;
+            obj.trl=obj.trl+1;
+        else
+            bSame=true;
+        end
+        s=obj.s;
+        int=obj.int;
+        trl=obj.trl;
+        if bSame && ~obj.bLastInit
+            opts=obj.cOpts;
+        else
+            opts=obj.getSubOpts(obj.int, obj.s, bInit);
+        end
+        obj.cOpts=opts;
+        obj.bLastInit=bInit;
+    end
+    function opts=getSubOpts(obj,int,subIntORname,bInit)
+        if nargin < 2 || isempty(int)
+            int=obj.int;
+        end
+        if nargin < 3 || isempty(subIntORname)
             subIntORname=1;
         end
-        opts=obj.Int{obj.nums==int};
+        if nargin < 4
+            bInit=0;
+        end
+
+        opts=obj.INTS{obj.nums==int};
+        flds=fieldnames(opts);
+
         if ischar(subIntORname)
             name=subIntORname;
+            s=find(ismember(flds,name));
         else
-            flds=fieldnames(opts);
-            name=flds{subIntORname};
+            s=subIntORname;
+            name=flds{s};
         end
         opts=opts.(name);
+        opts.name=name;
+        if bInit && ~isempty(opts.draw)
+            opts.reset=[opts.reset; opts.draw(~ismember(opts.draw,opts.reset))];
+        end
     end
 end
 methods(Access=private)
@@ -61,18 +111,18 @@ methods(Access=private)
         end
 
         n=length(intFlds);
-        obj.Int=cell(length(intFlds),1);
+        obj.INTS=cell(length(intFlds),1);
         mtch=regexp(intFlds,'[0-9]*','match');
         obj.nums=cellfun(@str2double,vertcat(mtch{:}));
         obj.nums=str2double(intFlds);
         for i = 1:n
             IntI=intOpts{intFlds{i}};
             flds=fieldnames(IntI);
-            Int{i}=struct();
+            INTS{i}=struct();
             for j = 1:length(flds)
                 IntIJ=IntI{flds{j}};
                 IntIJ=Args.parse([],P,IntIJ);
-                obj.Int{i}.(flds{j})=obj.parse_fun(IntIJ,P);
+                obj.INTS{i}.(flds{j})=obj.parse_fun(IntIJ,P);
             end
         end
 
@@ -81,13 +131,13 @@ methods(Access=private)
         %        IntH{h}=IntIJ;
         %    else
         %        n=n+1;
-        %        Int{n}=IntIJ;
+        %        INTS{n}=IntIJ;
         % end
         %end
         %obj.order=
         %obj.IntH=IntH;
-        %obj.Int=Int;
-        %obj.nInt=numel(obj.Int);
+        %obj.INTS=INTS;
+        %obj.nInt=numel(obj.INTS);
         %obj.nIntA=obj.nInt+numel(obj.nIntH);
     end
     function out=parse_fun(obj,int,P)
@@ -128,6 +178,8 @@ methods(Static)
             'loadt',0,'Num.isInt';                       % view
             ...
             'time',0,'@(x) true'; ...                   % view
+            'autoInc',1,'isbinary'; ...
+            'keyHold',0,'isbinary'; ...
             ...
             %'close','','@(x) iscell(x) || ischar(x)';
         };
